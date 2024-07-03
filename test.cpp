0534523ec530407e2e265bcd2ea2882560208244
aminulhaquekhan <156196374+aminulhaquekhan@users.noreply.github.com>
2024-07-04 00:22:12 +0600

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
-#include <iostream>
 #include <fstream>
-
 
 using namespace std;
 
@@ -42,4 +40,119 @@ bool adminLogin() {
         return false;
     }
 }
+
+void addItem() {
+    ofstream outFile("items.txt", ios::app);
+    if (!outFile) {
+        cout << "Error opening file." << endl;
+        return;
+    }
+
+    Item newItem;
+    cout << "\nEnter item code: ";
+    cin >> newItem.itemCode;
+    cin.ignore();
+    cout << "Enter item name: ";
+    getline(cin, newItem.itemName);
+    cout << "Enter item price: ";
+    cin >> newItem.itemPrice;
+    cout << "Enter quantity: ";
+    cin >> newItem.quantity;
+
+    outFile << newItem.itemCode << "," << newItem.itemName << ","
+            << newItem.itemPrice << "," << newItem.quantity << endl;
+
+    cout << "\nItem added successfully!\n";
+    outFile.close();
+}
+
+void displayItems() {
+    cout << "\n===== List of Items =====\n";
+    cout << "Code\tName\t\tPrice\tQuantity\n";
+    cout << "--------------------------------\n";
+
+    ifstream inFile("items.txt");
+    if (!inFile) {
+        cout << "Error opening file." << endl;
+        return;
+    }
+
+    Item item;
+    char comma;
+    while (inFile >> item.itemCode >> comma >> ws &&
+           getline(inFile, item.itemName, ',') &&
+           inFile >> item.itemPrice >> comma >> item.quantity) {
+        cout << item.itemCode << "\t" << setw(15) << left << item.itemName
+             << "\t" << item.itemPrice << "\t" << item.quantity << endl;
+    }
+
+    cout << "===========================\n";
+    inFile.close();
+}
+
+void generateBill() {
+    cout << "\n===== Generated Bill =====\n";
+    cout << "Code\tName\t\tPrice\tQuantity\tTotal\n";
+    cout << "---------------------------------------------\n";
+
+    ifstream inFile("items.txt");
+    if (!inFile) {
+        cout << "Error opening file." << endl;
+        return;
+    }
+
+    double totalBill = 0.0;
+    Item item;
+    char comma;
+    while (inFile >> item.itemCode >> comma >> ws &&
+           getline(inFile, item.itemName, ',') &&
+           inFile >> item.itemPrice >> comma >> item.quantity) {
+        double itemTotal = item.itemPrice * item.quantity;
+        cout << item.itemCode << "\t" << setw(15) << left << item.itemName
+             << "\t" << item.itemPrice << "\t" << item.quantity
+             << "\t" << itemTotal << endl;
+        totalBill += itemTotal;
+    }
+
+    cout << "---------------------------------------------\n";
+    cout << "\t\t\t\tTotal Bill: " << totalBill << endl;
+    cout << "=============================================\n";
+    inFile.close();
+}
+
+int main() {
+    int choice;
+
+    if (!adminLogin()) {
+        cout << "Login failed. Exiting program...\n";
+        return 0;
+    }
+
+    do {
+        showMenu();
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                addItem();
+                break;
+            case 2:
+                displayItems();
+                break;
+            case 3:
+                generateBill();
+                break;
+            case 4:
+                cout << "Exiting program...\n";
+                break;
+            default:
+                cout << "Invalid choice. Please enter a valid option.\n";
+                break;
+        }
+    } while (choice != 4);
+
+    return 0;
+}
+
 
